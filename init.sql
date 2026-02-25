1b2339fb2dcbb6e43213c0e0dcfa8067a34fecb3
unknown <matteo.lafrancesca@gmail.com>
2026-02-25 15:35:21 +0100

    retrait table etl_logs

--- init.sql
+++ init.sql
@@ -1,13 +1,5 @@
 -- Initial database setup
 -- Tables will be created here
-CREATE TABLE IF NOT EXISTS etl_logs (
-    id SERIAL PRIMARY KEY,
-    job_name VARCHAR(50),
-    status VARCHAR(20),
-    executed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    details TEXT
-);
-
 CREATE TABLE IF NOT EXISTS "Plan" (
     id_plan SERIAL PRIMARY KEY,
     gender VARCHAR(50),
